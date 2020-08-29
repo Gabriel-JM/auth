72e761b058a239037de0ec1b0d5d5e38df6e8af8
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-08-29 13:10:50 -0300

    Adjust some details

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import { app } from './main/app'
 import mongoose from 'mongoose'
 
-const dbUri = 'mongodb+srv://gabriel:gabriel@mycluster.yfxw4.mongodb.net/MyCluster?retryWrites=true&w=majority'
+const dbUri = 'mongodb+srv://gabriel:gabriel@mycluster.yfxw4.mongodb.net/node-auth'
 
 mongoose.connect(dbUri, {
   useNewUrlParser: true,
@@ -10,8 +10,8 @@ mongoose.connect(dbUri, {
 })
   .then(() => {
     console.clear()
-    console.log('Database Connected')
-    app.listen(3000, () => console.log('Server running...'))
+    console.log('\x1b[33m[INFO] Database Connected.\n', '\x1b[0m')
+    app.listen(3000, () => console.log('[Server] Running at Port: 3000'))
   })
   .catch(
     error => console.error(error)
