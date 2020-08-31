fb7eb2bebb5fe3b43f160c13da55d240a39f15c0
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-08-31 00:38:54 -0300

    Created UserModel and set signup route to create an user

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from 'express'
+import { UserInfo } from '../types/user-info'
+import UserModel from '../models/User'
 
 export function signupGet(req: Request, res: Response) {
   res.render('signup')
@@ -8,10 +10,21 @@ export function loginGet(req: Request, res: Response) {
   res.render('login')
 }
 
-export function signupPost(req: Request, res: Response) {
-  res.send('new signup')
+export async function signupPost(req: Request, res: Response) {
+  const { email, password } = req.body as UserInfo
+
+  try {
+    const user = await UserModel.create({ email, password })
+
+    res.status(201).json(user)
+  } catch(err) {
+    console.log('\x1b[31m[ERROR]\x1b[0m', err.message)
+    res.status(400).json('error, user not created.')
+  }
 }
 
 export function loginPost(req: Request, res: Response) {
+  const { email, password } = req.body as UserInfo
+
   res.send('user login')
 }
---
+++ src/models/User.ts
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  }
+})
+
+const UserModel = mongoose.model('user', userSchema)
+
+export default UserModel
