71031ae319b7c626be514a1bde33b9156afd1ee9
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-09-02 13:09:01 -0300

    Adding bcrypt to hash passwords

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose'
 import validator from 'validator'
+import bcrypt from 'bcrypt'
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -14,6 +15,13 @@ const userSchema = new mongoose.Schema({
     required: [true, 'Please enter an password'],
     minlength: [6, 'Mininum password length is 6 caracters']
   }
+})
+
+userSchema.pre('save', async function(this: any, next) {
+  const salt = await bcrypt.genSalt()
+  this.password = await bcrypt.hash(this.password, salt)
+
+  next()
 })
 
 const UserModel = mongoose.model('user', userSchema)
